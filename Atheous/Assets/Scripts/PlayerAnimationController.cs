0dcf9807e06fa6a3b9269945c4a3f208263c46a4
John McDonagh <johnmcdonaghsligo@gmail.com>
2018-02-26 15:41:40 +0000

    added player animation controller
    New script that makes animations change when key buttons are pressed. ex.  when right arrow is pushed change animation

---
+++ Atheous/Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private int direction = 0;
+    private int attack = 0;
+    private int defence = 0;
+    private int jump = 0;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        direction = 0;
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = 1;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            attack = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            defence = 1;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jump = 1;
+        }
+
+        
+    }
+}
