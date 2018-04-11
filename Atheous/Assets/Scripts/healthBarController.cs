b1022ae835ba6b56a12cf3b76be5afffe3d6f87d
healyum202 <36045537+healyum202@users.noreply.github.com>
2018-04-11 15:19:08 +0100

    added healthBar ui

---
+++ Atheous/Assets/Scripts/healthBarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+//Credit to Philip D' Arcy Y3 Games
+public class HealthBarController : MonoBehaviour
+{
+    private Image barImage;
+
+    void Start()
+    {
+        barImage = GetComponent<Image>();
+    }
+
+    public void UpdateHealth(float health)
+    {
+        barImage.fillAmount = health;
+    }
+}
