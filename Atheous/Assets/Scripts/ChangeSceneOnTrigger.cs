55c6fb82c1ac07435bf742a575a3f8340db33c4d
Sam Colville <scolville47@gmail.com>
2018-04-16 15:33:21 +0100

    Strt of scene management.

---
+++ Atheous/Assets/Scripts/ChangeSceneOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneTrigger : MonoBehaviour
+{
+
+    public string sceneName = "TheHub";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        string objectTag = collision.gameObject.tag;
+
+        if (objectTag == "Player")
+        {
+            if (!string.IsNullOrEmpty(sceneName))
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+}
---
+++ Atheous/Assets/Scripts/DoorController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DoorController : MonoBehaviour
+{
+
+
+    //Used for the hub doors to load levels and require a 
+    //key press to load level
+    public string sceneName = "TheWoods";
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        string objectTag = collision.gameObject.tag;
+
+        if (objectTag == "Player" && Input.GetKeyDown(KeyCode.X))
+        {
+            if (!string.IsNullOrEmpty(sceneName))
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+}    
