0d3b2a66f73f726cc7df584175e2051c9fe64a53
Sam Colville <scolville47@gmail.com>
2018-04-18 19:55:04 +0100

    misc cleanup

--- Atheous/Assets/Scripts/FloppDiscController.cs
+++ Atheous/Assets/Scripts/FloppDiscController.cs
@@ -2,16 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
 
-public class FloppDiscController : MonoBehaviour {
-=======
+
 public class FloppDiscController : MonoBehaviour 
 {
     PlayerController playerController;
     public int floppies;
 
->>>>>>> b673542706a0de4b292c24ff67b41b7c24c51f91
+
 
 	// Use this for initialization
 	void Start () 
