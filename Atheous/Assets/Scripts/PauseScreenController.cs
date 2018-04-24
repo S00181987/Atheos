dfc223a305d0739659dce0bf473fe9aafeae2353
Sam Colville <scolville47@gmail.com>
2018-04-24 23:38:57 +0100

    Pause screen
    added script for pause screen. need to add to canvas

---
+++ Atheous/Assets/Scripts/PauseScreenController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreenController : MonoBehaviour 
+{
+
+    public GameObject pauseMenu;
+
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(Input.GetKey(KeyCode.M))
+        {
+            Pause();
+        }
+		
+	}
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    void Exit()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+
+    public void Unpause()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+}
